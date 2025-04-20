27c7d00ee95c329d55779226c21b67e1ff9a1861
Cameron Kowalke <minikowalkes@gmail.com>
2025-04-20 00:31:22 -0400

    Updated test.py

--- src/test.py
+++ src/test.py
@@ -1,1 +1,9 @@
 # This is a test to see if I get an src directory
+def main():
+  msg = str("Hello World (of github)")
+  print(msg)
+
+if __name__ == "__main__":
+  main()
+# also the most convoluted hello world in python to my knowledge
+# but shows about how much I know.
