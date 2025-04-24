fe1c1077f93432f64b475282b9dde5bf6c4d3d50
Cameron Kowalke <minikowalkes@gmail.com>
2025-04-24 16:00:34 -0400

    Updated regex_v0.1.py
    should really be v0.2 but whatever

--- src/Python/regex_v0.1.py
+++ src/Python/regex_v0.1.py
@@ -1,3 +1,11 @@
 import re
 
+def filer(a: str):
+  '''take in story problem as string, write the file.'''
+  f = open("story.txt",'wt')
+  f.write(a)
+
+def searcher():
+  with story.txt as f:
+ '''brb'''
 # lol gotta learn this whole module
