da7e36d3b0d065f1d602a8d4e3b004b8aa2ec9d0
Primus Fan <minikowalkes@gmail.com>
2025-11-03 12:20:23 -0500

    Create chemTypeTest.cpp

---
+++ src/chemTypeTest.cpp
@@ -0,0 +1,12 @@
+// YOU THOUGHT I GAVE UP?!?!? I COULD NEVER. COULDNT BE ME.
+#include <iostream>
+#include <string>
+#include "chemType.h"
+
+using namespace std;
+
+int main() {
+	chemType heliumAtom("Helium", "He", 2, 4.0026);
+	heliumAtom.printChemType();
+	cout << endl;
+}
