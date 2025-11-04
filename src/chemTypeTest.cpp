ad11947d493bbd374429e436fcbb39b7183ea9d7
Primus Fan <minikowalkes@gmail.com>
2025-11-04 08:43:53 -0500

    Update chemTypeTest.cpp

--- src/chemTypeTest.cpp
+++ src/chemTypeTest.cpp
@@ -1,12 +1,21 @@
-// YOU THOUGHT I GAVE UP?!?!? I COULD NEVER. COULDNT BE ME.
+// GUESS WHO FIXED MOLETYPE
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include "chemType.h"
+#include <cmath>
+#include "moleType.h"
 
 using namespace std;
 
 int main() {
-	chemType heliumAtom("Helium", "He", 2, 4.0026);
-	heliumAtom.printChemType();
+	//chemType heliumAtom();
+	//heliumAtom.printChemType();
+	// oh my god the moleType is actually driving me nuts. I'm going to have to rewrite this constructor a billion times.
+	moleType heliumMol("mol", 2.35,"Helium", "He", 2, 4.0026);
+	heliumMol.printChemType();
+	cout << endl;
+	heliumMol.printMoleType();
+	
+
 	cout << endl;
 }
